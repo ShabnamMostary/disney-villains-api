58893074ead45acd8da6012d53d553f3d63d9a83
shabnam <shabnam.mostary@gmail.com>
2020-11-25 23:45:22 -0500

    created mock folder & added file with test data

--- tests/controllers/index.test.js
+++ tests/controllers/index.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+const models = require('../../models')
 const { describe, it } = require('mocha')
 const { getAllVillains, getVillainBySlug, addNewVillain } = require('../../controllers/index')
 
@@ -8,7 +9,15 @@ chai.use(sinonChai)
 const { expect } = chai
 
 describe('Controllers - disneyVillainsApi', () => {
-  describe('getAllVillains', () => { })
+  describe('getAllVillains', () => {
+    it('retrieves a list of disney villains from the database and calls response.send() with the list', async () => {
+      const stubbedFindAll = sinon.stub(models.villains, 'findAll')
+      const stubbedSend = sinon.stub()
+      const response = { send: stubbedSend }
+
+      await getAllVillains({}, response)
+    })
+  })
   describe('getVillainBySlug', () => { })
   describe('addNewVillain', () => { })
 })
---
+++ tests/mocks/villains.js
@@ -0,0 +1,16 @@
+const villainsList = [{
+  name: 'Captain Hook',
+  movie: 'Peter Pan',
+  slug: 'captain-hook',
+}, {
+  name: 'Cruella de Vil',
+  movie: 'One Hundred and One Dalmatians',
+  slug: 'cruella-de-vil',
+}]
+const singleVillain = {
+  name: 'Jafar',
+  movie: 'Aladdin',
+  slug: 'jafar',
+}
+
+module.exports = { villainsList, singleVillain }
